b0e00d17e5c1e0fb6b639d622ea0721cffdc97ab
SherZahed <sherzahed@yahoo.com>
2014-02-09 02:28:45 -0800

    Update Coord.java

--- Coord.java
+++ Coord.java
@@ -1,23 +1,52 @@
+import objectdraw.*;
+import java.awt.*;
+
 public class Coord
 {
     private boolean available;
-    private filledOval areaTester;
-    private filledOval selectionBubble;
+    private FilledOval areaTester;
+    private FilledOval selectionBubble;
     private double xCoord;
     private double yCoord;
-    private int areaRadius;
+    private double areaRadius;
     private int selectionRadius;
-    
+
     public Coord(Location aLocation, DrawingCanvas canvas)
     {
         xCoord = aLocation.getX();
         yCoord = aLocation.getY();
-        areaRadius = LENGTH/2;
+        areaRadius = Hexagon.LENGTH/2;
         selectionRadius = 15;
-        areaTester = new filledOval(xCoord - areaRadius, yCoord - areaRadius, LENGTH, LENGTH, canvas);
+        areaTester = new FilledOval(xCoord - areaRadius, yCoord - areaRadius, Hexagon.LENGTH+11, Hexagon.LENGTH+11, canvas);
+        areaTester.setColor(new Color(0, 189, 0, 125));
         areaTester.hide();
-        selectionBubble = new filledOval(xCoord - selectionRadius, yCoord - selectionRadius, selectionRadius*2, selectionRadius*2, canvas);
+        selectionBubble = new FilledOval(xCoord - selectionRadius, yCoord - selectionRadius, selectionRadius*2, selectionRadius*2, canvas);
+        selectionBubble.setColor(new Color(0,0, 255, 125));
         available = true;
         selectionBubble.hide();
     }
-}
+
+    public void showSelectionRadius()
+    {
+        selectionBubble.show();
+    }
+
+    public void hideSelectionBubble()
+    {
+        selectionBubble.hide();
+    }
+
+    public void showAreaTester()
+    {
+        areaTester.show();
+    }
+
+    public void hideAreaTester()
+    {
+        areaTester.hide();
+    }
+
+    public void changeTesterColor(Color colour)
+    {
+        areaTester.setColor(colour);
+    }
