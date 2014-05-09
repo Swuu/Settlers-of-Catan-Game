d9dd5b2232abe9b2743e69d9e3bcec7bc947a344
Eric Cheng <ericcheng95@gmail.com>
2014-05-08 23:09:04 -0700

    Included Artwork into Settlements and Citys!
    WOOP DE FUCKING HOO. FUCK YEAHHHHHHHH

--- SettlementShape.java
+++ SettlementShape.java
@@ -4,79 +4,84 @@ import java.awt.*;
 // Makes a house, with the middle of the square at x , y
 public class SettlementShape
 {
-	private FilledRect base, window;
-	private FramedRect frame1, frame2, cityRect;
-    private Triangle roof;
     private int playerNum;
-    private DrawingCanvas cnv;
-    private Line cityLine;
+    private VisibleImage base;
+    private Toolkit toolkit;
+    private DrawingCanvas canvas;
 
     /* CONFIG */
-    private double width = 24;
-    private double height = 20;
+    private double width = 40;
+    private double height = 40;
     
     private double xCoord, yCoord;
     
-	public SettlementShape (double x , double y , int aNum , DrawingCanvas canvas)
+	public SettlementShape (double x , double y , int aNum , DrawingCanvas aCanvas)
     {
+        toolkit = Toolkit.getDefaultToolkit();
         xCoord = x;
         yCoord = y;
-        cnv = canvas;
-        Constructor();
+        canvas = aCanvas;
         playerNum = aNum;
-        setColor();
+        makeSettlement();
     }
 
-    public SettlementShape (Location aLocation, int aNum , DrawingCanvas canvas)
+    public SettlementShape (Location aLocation, int aNum , DrawingCanvas aCanvas)
     {
+        toolkit = Toolkit.getDefaultToolkit();
         xCoord = aLocation.getX();
         yCoord = aLocation.getY();
-        
-        cnv = canvas;
-        Constructor();
+        canvas = aCanvas;
         playerNum = aNum;
-        setColor();
+        makeSettlement();
     }
     
-    public void Constructor()
+    public void makeSettlement()
     {
-        frame1 = new FramedRect(xCoord-width/2 -1, yCoord-height/2 -1, width+1, height+1, cnv);
-        base = new FilledRect(xCoord-width/2, yCoord-height/2, width, height, cnv);
-        frame2 = new FramedRect(xCoord-width/6 -1, yCoord-height/6 -1, width/3 +1,
-                                height/3 +1, cnv);
-        window = new FilledRect(xCoord-width/6, yCoord-height/6, width/3, height/3,
-                                cnv);
-        roof = new Triangle(xCoord-width/2, yCoord-height/2, width, height, cnv);
-        
-        window.setColor(Color.YELLOW);
+        double x = xCoord - width/2;
+        double y = yCoord - height/2;
+        switch (playerNum)
+        {
+            case 0:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationNeutralBase.png"), x, y, 41, 41, canvas);
+                break;
+            case 1:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationRedBase.png"), x, y, 41, 41, canvas);
+                break;
+            case 2:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationBlueBase.png"), x, y, 41, 41, canvas);
+                break;
+            case 3:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationOrangeBase.png"), x, y, 41, 41, canvas);
+                break;
+            case 4:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationPurpleBase.png"), x, y, 41, 41, canvas);
+                break;
+        }
+
     }
 
     public void makeCity()
     {
-        cityRect = new FramedRect(xCoord-width/2 - 2, yCoord-height/2 - 2, width + 2, height + 2, cnv);
-        cityLine = new Line(xCoord-width/2, yCoord, xCoord+width/2, yCoord, cnv);
-    }
-    
-    public void setColor()
-    {
-        Color color = Color.BLACK;
+        double x = xCoord - width/2;
+        double y = yCoord - height/2;
         switch (playerNum)
         {
             case 0:
-                color = Color.GRAY;
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationNeutral.png"), x, y, 41, 41, canvas);
                 break;
             case 1:
-                color = Color.RED;
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationRed.png"), x, y, 41, 41, canvas);
                 break;
             case 2:
-                color = Color.BLUE;
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationBlue.png"), x, y, 41, 41, canvas);
                 break;
             case 3:
-                color = Color.GREEN;
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationOrange.png"), x, y, 41, 41, canvas);
+                break;
+            case 4:
+                base = new VisibleImage(toolkit.getImage("image/SpaceStationPurple.png"), x, y, 41, 41, canvas);
                 break;
         }
-        base.setColor(color);
-        roof.tr_setColor(color);
     }
             
     public void moveTo(Location aLocation)
@@ -89,41 +94,22 @@ public class SettlementShape
     
     public void moveTo(double x, double y)
     {
-        double preX = base.getX();
-        double preY = base.getY();
-        
-        frame1.moveTo(x - width/2 - 1, y - height/2 - 1);
-        frame2.moveTo(x - width/6 - 1, y - height/6 - 1);
         base.moveTo(x - width/2, y - height/2);
-        window.moveTo(x - width/6, y - height/6);
-        roof.moveTo(x - 1 - width/2, y - 1 - height/2);
     }
     
     public void sendToFront()
     {
-        frame1.sendToFront();
         base.sendToFront();
-        frame2.sendToFront();
-        window.sendToFront();
-        roof.tr_sendToFront();
     }
     
     public void hide()
     {
-        frame1.hide();
         base.hide();
-        frame2.hide();
-        window.hide();
-        roof.tr_hide();
     }
         
     public void show()
     {
-        frame1.show();
         base.show();
-        frame2.show();
-        window.show();
-        roof.tr_show();
     }
 
 } 
